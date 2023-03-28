6a1517e28b6d2b23f3b46890a55d8edb4cbab72d
Soporte TI <Soporte TI@TI-LAP01.newportcorp.local>
2023-03-28 15:02:37 -0600

    Modificaciones Fabian

--- src/funcionesJuego/Images.java
+++ src/funcionesJuego/Images.java
@@ -1,54 +1,84 @@
 /* Esta clase se encarga de traer la imagenes desde la carpeta hasta el programa*/
-
 package funcionesJuego;
 
 import javax.swing.ImageIcon;
 
 public class Images {
-                
-    public ImageIcon getMain_Character_Walking(int i, String action, String type){ // Segun accion, numero de sequencia y tipo de personaje se escoge la imagen.
+
+    public ImageIcon getMain_Character_Walking(int i, String action, String type) { // Segun accion, numero de sequencia y tipo de personaje se escoge la imagen.
         ImageIcon retorno = null;
         String path = null;
-            switch(action){
-                case "Walking_Right":
-                    path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Walking\\Walking_"));
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Walking_Left": 
-                    path = ".\\src\\funcionesJuego\\Principal\\Izquierda\\Walking\\Satyr_01_Walking_";
-                    retorno =  new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Jumping_Right":
-                    path = ".\\src\\funcionesJuego\\Principal\\Derecha\\Jump_Loop\\Satyr_01_Jump_Loop_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Jumping_Left":
-                    path = ".\\src\\funcionesJuego\\Principal\\Izquierda\\Jump_Loop\\Satyr_01_Jump_Loop_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Attacking_Left":
-                    path = ".\\src\\funcionesJuego\\Principal\\Izquierda\\Attacking\\Satyr_01_Attacking_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Attacking_Right":
-                    path = ".\\src\\funcionesJuego\\Principal\\Derecha\\Attacking\\Satyr_01_Attacking_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Idle_Right":
-                    path = ".\\src\\funcionesJuego\\Principal\\Derecha\\Idle\\Satyr_01_Idle_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                case "Idle_Left":
-                    path = ".\\src\\funcionesJuego\\Principal\\Izquierda\\Idle\\Satyr_01_Idle_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-                default: 
-                    path = ".\\src\\funcionesJuego\\Principal\\Derecha\\Walking\\Satyr_01_Walking_";
-                    retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
-                    break;
-            } 
- 
-    
+        switch (action) {
+            case "Walking_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Walking\\Walking_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Walking_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Walking\\Walking_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Jump_Loop_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Jump_Loop\\Jump_Loop_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Jump_Loop_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Jump_Loop\\Jump_Loop_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Jump_Start_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Jump_Start\\Jump_Start_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Jump_Start_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Jump_Start\\Jump_Start_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Attacking_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Attacking\\Attacking_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Attacking_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Attacking\\Attacking_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Attacking2_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Attacking2\\Attacking2_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Attacking2_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Attacking2\\Attacking2_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Idle_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Idle\\Idle_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Idle_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Idle\\Idle_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Hurt_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Hurt\\Hurt_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Hurt_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Hurt\\Hurt_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Dying_Right":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Dying\\Dying_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            case "Dying_Left":
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Izquierda\\Dying\\Dying_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+            default:
+                path = ".\\src\\funcionesJuego\\".concat(type.concat("\\Derecha\\Walking\\Walking_"));
+                retorno = new ImageIcon(path.concat(String.valueOf(i).concat(".png")));
+                break;
+        }
+
         return retorno;
     }
 }
